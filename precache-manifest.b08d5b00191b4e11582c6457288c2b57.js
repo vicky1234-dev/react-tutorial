6b198fc2305b5e29e5b73709df8899a8568da4a5
vickysingh12 <vicky.bhullar1000@gmail.com>
2020-10-14 17:56:08 +0530

    Updates

---
+++ precache-manifest.b08d5b00191b4e11582c6457288c2b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1c804bb107a8e453ba8723318f82e9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "1a1d1e5133979c223bb5",
+    "url": "/react-tutorial/static/css/main.53574754.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1d1e5133979c223bb5",
+    "url": "/react-tutorial/static/js/main.4430624e.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
